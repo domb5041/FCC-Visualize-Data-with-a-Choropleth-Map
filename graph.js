13f5a1c79ee06668e63aca3cbee7450cc6018b67
dominicbutler <dominic.butler@exonar.com>
2021-02-25 17:38:36 +0000

    removed commented code

--- graph.js
+++ graph.js
@@ -72,65 +72,6 @@ function ready(error, us, education) {
             tooltip.style('opacity', 0);
         });
 
-    // const xScale = d3
-    //     .scaleBand()
-    //     .domain(data.monthlyVariance.map(d => d.year))
-    //     .range([p, w - p]);
-
-    // const yScale = d3
-    //     .scaleBand()
-    //     .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-    //     .range([p, h - p]);
-
-    // const cScale = d3
-    //     .scaleThreshold()
-    //     .domain([-4, -2, 0, 2, 4])
-    //     .range(['blue', 'lightblue', 'yellow', 'orange', 'red']);
-
-    // const xAxis = d3
-    //     .axisBottom(xScale)
-    //     .tickValues(xScale.domain().filter(year => year % 20 === 0))
-    //     .tickFormat(year => d3.timeFormat('%Y')(new Date(year, 0, 1)));
-
-    // const yAxis = d3
-    //     .axisLeft(yScale)
-    //     .tickFormat(month => d3.timeFormat('%B')(new Date(1970, month, 1)));
-
-    // svg.append('g')
-    //     .attr('transform', `translate(${p}, 0)`)
-    //     .attr('id', 'y-axis')
-    //     .call(yAxis);
-
-    // svg.append('g')
-    //     .attr('transform', `translate(0, ${h - p})`)
-    //     .attr('id', 'x-axis')
-    //     .call(xAxis);
-
-    // svg.selectAll('rect')
-    //     .data(data.monthlyVariance)
-    //     .enter()
-    //     .append('rect')
-    //     .attr('x', d => xScale(d.year))
-    //     .attr('y', d => yScale(d.month - 1))
-    //     .attr('width', xScale.bandwidth())
-    //     .attr('height', yScale.bandwidth())
-    //     .attr('class', 'cell')
-    //     .attr('data-month', d => d.month - 1)
-    //     .attr('data-year', d => d.year)
-    //     .attr('data-temp', d => d.variance)
-    //     .style('fill', d => cScale(d.variance))
-    //     .on('mouseover', d => {
-    //         svg.append('text')
-    //             .text(d.year)
-    //             .attr('id', 'tooltip')
-    //             .attr('x', xScale(d.year) + 10)
-    //             .attr('y', yScale(d.month) + 5)
-    //             .attr('data-year', d.year);
-    //     })
-    //     .on('mouseout', () => {
-    //         d3.selectAll('#tooltip').remove();
-    //     });
-
     const legend = svg.append('g').attr('id', 'legend');
 
     legend
